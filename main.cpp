97aab9e96dfc132a3e4691949bf2b9f9a82b2a0c
Niko <nicolo.carcagni@mail.com>
2022-10-24 22:16:36 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    fstream strm;
+    time_t currentTime;
+    time(&currentTime);
+    strm.open("/Volumes/ASSD/files.txt", ios_base::out); //'out' sovrascrive, 'app' aggiunge
+    if(strm.is_close()){
+        cout << "Error 100, file could be opened." << endl; 
+        return 1;
+    }
+    strm << asctime(localtime(&currentTime)) << endl;
+    strm.close();
+    if(strm.is_open()){
+        cout << "Error 101, file could not be opened." << endl;
+        return 1;
+    }
+    cout << "Completed." << endl;
+    return 0;
+}
